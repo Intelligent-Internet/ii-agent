e83d3b9ae53b94f03fa99b25a3f2c70bb89a7b61
Khoa Ngo The <khoa@ii.inc>
2025-05-12 18:04:03 +0700

    feat: refactor

--- src/ii_agent/tools/__init__.py
+++ src/ii_agent/tools/__init__.py
@@ -1,12 +1,8 @@
-from ii_agent.tools.web_search import DuckDuckGoSearchTool
-from ii_agent.tools.visit_webpage import VisitWebpageTool
-from ii_agent.tools.tavily_web_search import TavilySearchTool
-from ii_agent.tools.tavily_visit_webpage import TavilyVisitWebpageTool
 from ii_agent.tools.str_replace_tool import StrReplaceEditorTool
-from ii_agent.tools.static_deploy_tool import StaticDeployTool
 from ii_agent.tools.sequential_thinking_tool import SequentialThinkingTool
-from ii_agent.tools.complete_tool import CompleteTool
-from ii_agent.tools.bash_tool import create_bash_tool, create_docker_bash_tool, BashTool
+from ii_agent.tools.bash_tool import BashTool
+from ii_agent.tools.tavily_visit_webpage import TavilyVisitWebpageTool
+from ii_agent.tools.tool_manager import get_system_tools, AgentToolManager
 
 # Tools that need input truncation (ToolCall)
 TOOLS_NEED_INPUT_TRUNCATION = {
@@ -19,17 +15,8 @@ TOOLS_NEED_INPUT_TRUNCATION = {
 TOOLS_NEED_OUTPUT_FILE_SAVE = {TavilyVisitWebpageTool.name}
 
 __all__ = [
-    "DuckDuckGoSearchTool",
-    "VisitWebpageTool",
-    "TavilySearchTool",
-    "TavilyVisitWebpageTool",
-    "StrReplaceEditorTool",
-    "StaticDeployTool",
-    "SequentialThinkingTool",
-    "CompleteTool",
-    "BashTool",
-    "create_bash_tool",
-    "create_docker_bash_tool",
+    "AgentToolManager",
     "TOOLS_NEED_INPUT_TRUNCATION",
     "TOOLS_NEED_OUTPUT_FILE_SAVE",
+    "get_system_tools",
 ]
